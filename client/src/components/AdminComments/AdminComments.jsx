81e70eff248124193f605b909236d701121eea94
N-Avramova <newenka@abv.bg>
2025-12-03 23:58:59 +0200

    Get comments for approve dynamically

--- client/src/components/AdminComments/AdminComments.jsx
+++ client/src/components/AdminComments/AdminComments.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { fetchNotApprovedComments } from "../../services/commentsService";
+import { fetchUsersByUserIds } from "../../services/usersServices";
+
 export default function AdminComments() {
-  const comments = [
-    { id: 1, text: "This is a great post!", owner: "Alice" },
-    { id: 2, text: "Needs more details.", owner: "Bob" },
-    { id: 3, text: "I agree with the points made.", owner: "Charlie" },
-  ];
+  const [commentsData, setCommentsData] = useState([]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const comments = await fetchNotApprovedComments();
+       const commentsOwnersData = await fetchUsersByUserIds(comments.map(c => c._ownerId));
+                  const resultComments = comments.map(comment => {
+                      const owner = commentsOwnersData.find(owner => owner.id === comment.ownerId);
+                      return {
+                          ...comment,
+                          commentOwnerFullName: [owner?.firstName, owner?.lastName].filter(Boolean).join(' ') || '',
+                          email: owner?.email || ''
+                      };
+                  });
+
+      setCommentsData(resultComments);
+    };
+    fetchComments();
+  }, []);
 
   return (
     <div className="p-6 flex justify-center">
@@ -17,6 +35,9 @@ export default function AdminComments() {
               <th className="px-6 py-3 text-left text-sm font-semibold text-gray-700">
                 Owner
               </th>
+               <th className="px-6 py-3 text-left text-sm font-semibold text-gray-700">
+                Email
+              </th>
               <th className="px-6 py-3 text-center text-sm font-semibold text-gray-700">
                 Actions
               </th>
@@ -24,10 +45,11 @@ export default function AdminComments() {
           </thead>
 
           <tbody className="divide-y divide-gray-200">
-            {comments.map((c) => (
+            {commentsData.map((c) => (
               <tr key={c.id}>
                 <td className="px-6 py-4 text-gray-800">{c.text}</td>
-                <td className="px-6 py-4 text-gray-500">{c.owner}</td>
+                <td className="px-6 py-4 text-gray-500">{c.commentOwnerFullName}</td>
+                <td className="px-6 py-4 text-gray-500">{c.email}</td>
                 <td className="px-6 py-4 flex items-center justify-center gap-2">
                   <button className="px-3 py-1.5 bg-green-600 text-white text-sm rounded hover:bg-green-700 transition">
                     Approve
--- client/src/services/commentsService.js
+++ client/src/services/commentsService.js
@@ -6,6 +6,14 @@ export async function fetchComments() {
     return Object.values(data);
 }
 
+export async function fetchNotApprovedComments() {
+    const response = await fetch(`${baseUrl}/jsonstore/comments`);
+    const data = await response.json();
+    const allComments = Object.values(data);
+    const notApprovedComments = allComments.filter(c => !c.isApproved);
+    return notApprovedComments;
+}
+
 export async function createComment(data) {
     const response = await fetch(`${baseUrl}/jsonstore/comments`, {
         method: 'POST',
