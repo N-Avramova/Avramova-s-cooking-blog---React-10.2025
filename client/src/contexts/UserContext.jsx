b71b1aec1f586571de798fd757c28ed2703944f2
N-Avramova <newenka@abv.bg>
2025-12-08 10:55:52 +0200

    Commit registerHandler for the Register component

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import { createContext } from 'react';
-import { fetchAllUsers } from '../services/usersServices'; 
+import useRequest from '../hooks/useRequest';
+
+const initialState = {
+    email: '',
+    password: '',
+    _createdOn: 0,
+    _id: '',
+    accessToken: ''
+};
 
 const UserContext = createContext({
     isAuthenticated: false,
-    user: null,
+    user: initialState,
+    registerHandler() { },
     loginHandler: () => { },
     logoutHandler: () => { },
 });
@@ -13,13 +22,43 @@ export function UserProvider({
     children,
 }) {
     const [user, setUser] = useState(null);
+    const { requestData } = useRequest();
+
+    const registerHandler = async (email, password) => {
+        const isAdmin = false;
+        const newUser = { email, password, isAdmin };
+
+        // Register API call 
+        const user = await requestData('users/register', 'POST', newUser);
+
+        // Login user after register and save token
+        setUser(user);
+    };
 
     const loginHandler = async (email, password) => {
-        const allUsers = await fetchAllUsers();
-        console.log(`Logging in with Email: ${email}, Password: ${password}`);
 
-        const user = allUsers.find(user => user.email === email && user.password === password);
+        // work 
+        const user = await requestData("users/login", "POST", { email, password })
+        // email = "peter@abv.bg";
+        // password ="123456";
+        //  // Register API call 
+        // const response = await fetch('http://localhost:3030/users/login', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json',
+        //     },
+        //     body: JSON.stringify({ email, password }),
+        // });
+
+        // const result = await response.json();
+
+
+        // const allUsers = await fetchAllUsers();
+        // console.log(`Logging in with Email: ${email}, Password: ${password}`);
+
+        // const user = allUsers.find(user => user.email === email && user.password === password);
         setUser(user);
+
     };
 
     const logoutHandler = async () => {
@@ -27,8 +66,10 @@ export function UserProvider({
     };
 
     const userContextValue = {
-        isAuthenticated: !!user?.isAdmin,
+        isAuthenticated: !!user.accessToken,
+        isAdmin: user?.isAdmin,
         user,
+        registerHandler,
         loginHandler,
         logoutHandler,
     };
